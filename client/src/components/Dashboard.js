02a83e7369a015030f0d9a0c581408a3db39927f
Abdou00 <abdoulaye_diarra@outlook.fr>
2024-02-22 10:30:55 +0100

    finish dashboard view

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import TimezoneSelect from "react-timezone-select";
 import { useNavigate } from "react-router-dom";
+import { time } from "../utils/ressource";
+import { toast } from "react-toastify";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
   const [selectedTimezone, setSelectedTimezone] = useState({});
   const [schedule, setSchedule] = useState([
     {day: "Sun", startTime: "", endTime: ""},
@@ -13,8 +16,24 @@ const Dashboard = () => {
     {day: "Fri", startTime: "", endTime: ""},
     {day: "Sat", startTime: "", endTime: ""},
   ]);
-  
-  const navigate = useNavigate();
+
+  // Màj du tableau des horaires de début et de fin
+  const handleTimeChange = (e, id) => {
+    const { name, value } = e.target;
+
+    if (value === "Select time") return;
+
+    const list = [...schedule];
+    console.log(list);
+    list[id][name] = value;
+
+    setSchedule(list);
+  }
+
+  const handleSaveSchedules = () => {
+    if (JSON.stringify(selectedTimezone) !== "{}") console.log(schedule);
+    else toast.error("Select your timezone")
+  };
 
   // Purge le local storage à la déconnexion
   const handleLogout = () => {
@@ -40,6 +59,46 @@ const Dashboard = () => {
             value={selectedTimezone}
             onchange={selectedTimezone}
           />
+
+          { schedule.map((sch, id) => (
+            <div className="form" key={id}>
+              <p>{ sch.day }</p>
+
+              <div className="select__wrapper">
+                <label htmlFor="startTime">Start Time</label>
+                <select
+                  name="startTime"
+                  id="startTime"
+                  onchange={(e) => handleTimeChange(e, id)}
+                >
+                  { time.map((t) => (
+                    <option key={id} id={t.id} value={t.hour}>
+                      { t.hour }
+                    </option>
+                  )) }
+                </select>
+              </div>
+
+              <div className="select__wrapper">
+                <label htmlFor="endTime">End Time</label>
+                <select
+                  name="endTime"
+                  id="endTime"
+                  onchange={(e) => handleTimeChange(e, id)}
+                >
+                  { time.map((t) => (
+                    <option key={id} id={t.id} value={t.hour}>
+                      { t.hour }
+                    </option>
+                  )) }
+                </select>
+              </div>
+            </div>
+          )) }
+        </div>
+
+        <div className="saveBtn__container">
+          <button onClick={handleSaveSchedules}>Save</button>
         </div>
       </main>
     </div>
